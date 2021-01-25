efbfad9bee2d065900ed6544ffe64633fd64657b
Yuriy Bergrin <b>
2021-01-25 21:33:01 +0300

    занятие от 25 января 20221

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -10,9 +10,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class SampleTest {
 	//неявные ожидание в классе driver factory
@@ -34,7 +37,7 @@ public class SampleTest {
 		driver = WebDriverFactory.create(webDriverBrowserName);
 		driver.get("https://msk.tele2.ru/shop/number");
 		driver.findElement(By.id("searchNumber")).sendKeys("97");
-		wait = new WebDriverWait(driver,10);
+		wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("catalog-numbers")));
 	}
 
@@ -47,6 +50,46 @@ public class SampleTest {
 		driver.get("https://yandex.ru");
 		Assert.assertEquals("Яндекс", driver.getTitle());
 	}
+
+	@Test
+	public void testBootStrap() throws InterruptedException {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		//инициализируем драйвер с опциями
+		driver = WebDriverFactory.create(webDriverBrowserName, options);
+
+		final String url = "https://ng-bootstrap.github.io/#/components/alert/examples ";
+		final String ERROR_MESSAGE = "Message equal";
+		String button = "//button[contains(text(),'Change message')]";
+		String alertTextBefore;
+		String alertTextAfter;
+
+		driver.get(url);
+		logger.info(String.format("Открыта станица %s", url));
+		WebElement element = driver.findElement(By.xpath(button));
+		logger.info("Кнопка найдена");
+
+		alertTextBefore = getAlertText(element);
+		logger.info(String.format("Alert %s", alertTextBefore));
+
+		Thread.sleep(1500);
+
+		alertTextAfter = getAlertText(element);
+		logger.info(String.format("Alert %s", alertTextAfter));
+
+		Assert.assertNotEquals(alertTextBefore, alertTextAfter, ERROR_MESSAGE);
+
+	}
+
+	private String getAlertText(WebElement element) {
+		String alert = "//ngb-alert[contains(text(), 'Message successfully changed')]";
+		element.click();
+		WebElement alertBox = driver.findElement(By.xpath(alert));
+		new WebDriverWait(driver, 4).until(visibilityOf(alertBox));
+		return alertBox.getText();
+	}
+
 
 	@After
 	public void setDown() {
