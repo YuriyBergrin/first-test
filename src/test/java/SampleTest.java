fec2c0b375df6ebfd681766623849cbb5f20a19a
Yuriy Bergrin <b>
2021-01-26 21:39:45 +0300

    home work

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -14,38 +14,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+
 public class SampleTest {
-	//неявные ожидание в классе driver factory
 	private Logger logger = LogManager.getLogger(SampleTest.class);
 	private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 	protected static WebDriver driver;
-	private String webDriverBrowserName;
-	private WebDriverWait wait;//явное ожидание
 
 	@Before
 	public void setUp() {
-		webDriverBrowserName = System.getProperty("browser");//драйвер задается командой
-		// например clean test -Dbrowser='ChRomE'
-		//если просто clean test то по умолчанию будет firefox, указал в помнике
-	}
-
-	@Test//без опций с явным ожиданием
-	public void webDriverFactoryTest() {
-		driver = WebDriverFactory.create(webDriverBrowserName);
-		driver.get("https://msk.tele2.ru/shop/number");
-		driver.findElement(By.id("searchNumber")).sendKeys("97");
-		wait = new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("catalog-numbers")));
-	}
-
-	@Test//с опциями
-	public void webDriverFactoryWithOptionsTest() {
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("start-maximized");
-		//инициализируем драйвер с опциями
-		driver = WebDriverFactory.create(webDriverBrowserName, options);
-		driver.get("https://yandex.ru");
-		Assert.assertEquals("Яндекс", driver.getTitle());
+//		options.addArguments()
+		driver = WebDriverFactory.create(CHROME);
+	}
+
+
+	@Test
+	public void yandexMarketTest() {
+		driver.get(cfg.urlYandex());
 	}
 
 	@After
