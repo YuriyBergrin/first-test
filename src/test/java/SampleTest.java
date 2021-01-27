7a5b52288a24a604a6df60d5e7d70666ee8bfbbc
Yuriy Bergrin <b>
2021-01-27 12:41:08 +0300

    home work

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,6 +1,5 @@
 import config.ServerConfig;
 import driverfactory.WebDriverFactory;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +10,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,19 +19,58 @@ import static org.openqa.selenium.remote.BrowserType.CHROME;
 public class SampleTest {
 	private Logger logger = LogManager.getLogger(SampleTest.class);
 	private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
-	protected static WebDriver driver;
+	private static WebDriver driver;
+	private Actions actions;
+	private WebDriverWait wait;
+	//локаторы
+	private By electronics = By.xpath("//span[contains(text(),\"Электроника\")]");
+	private By smartphones = By.xpath("//a[contains(text(),\"Смартфоны\")]");
+	private By samsung = By.xpath("//span[text() = \"Samsung\"]");
+	private By xiaomi = By.xpath("//span[text() = \"Xiaomi\"]");
+	private By sortByPrice = By.cssSelector("[data-autotest-id=\"dprice\"]");
+	private By firstSamsung = By.xpath("//span[contains(text(),\"Samsung\")]/../../../../.." +
+			"//div[contains(@aria-label,\"сравнению\")]");
+	private By accept = By.xpath("//*[text()=\"Понятно\"]/..");
+	private By added = By.xpath("//*[contains(text(),\"добавлен к сравнению\")]");
+	private By firstXiaomi = By.xpath("//span[contains(text(),\"Xiaomi\")]/../../../../.." +
+			"//div[contains(@aria-label,\"сравнению\")]");
+	private By compare = By.xpath("//span[text() = \"Сравнить\"]/..");
+	private By elementInList = By.xpath("//*[@class = \"LwwocgVx0q _2VGDFjE-Ev\"]");
+
 
 	@Before
 	public void setUp() {
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments()
-		driver = WebDriverFactory.create(CHROME);
+		options.addArguments("start-maximized");
+		driver = WebDriverFactory.create(CHROME, options);
+		actions = new Actions(driver);
+		wait = new WebDriverWait(driver, 5);
 	}
-
-
+	
 	@Test
 	public void yandexMarketTest() {
 		driver.get(cfg.urlYandex());
+		driver.findElement(electronics).click();
+		driver.findElement(smartphones).click();
+		driver.findElement(smartphones).click();
+		driver.findElement(samsung).click();
+		driver.findElement(xiaomi).click();
+		driver.findElement(sortByPrice).click();
+		driver.findElement(accept).click();
+		moveToElement(firstSamsung);
+//		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(firstSamsung)));//не помогает,
+//		не знаю какое условие подобрать, чтобы не использовать sleep
+		pause(2);
+		driver.findElement(firstSamsung).click();
+		Assert.assertTrue(driver.findElement(added).getText().contains("Samsung"));
+//		xiaomi
+		moveToElement(firstXiaomi);
+		pause(2);
+		driver.findElement(firstXiaomi).click();
+		Assert.assertTrue(driver.findElement(added).getText().contains("Xiaomi"));
+		//переход в сравнение
+		wait.until(ExpectedConditions.elementToBeClickable(compare)).click();
+		Assert.assertEquals(2, driver.findElements(elementInList).size());
 	}
 
 	@After
@@ -40,5 +79,17 @@ public class SampleTest {
 			driver.quit();
 			logger.info("Драйвер успешно закрыт");
 		}
+	}
+
+	private void pause(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void moveToElement(By by) {
+		actions.moveToElement(driver.findElement(by)).build().perform();
 	}
 }
