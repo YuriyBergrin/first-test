8e915e816ab409029a5f6ae4c8c4304f077c302f
Yuriy Bergrin <b>
2021-02-03 12:05:27 +0300

    add base test class edit test edit driver factory

--- src/main/java/driverfactory/WebDriverFactory.java
+++ src/main/java/driverfactory/WebDriverFactory.java
@@ -12,19 +12,22 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverFactory {
-	private static WebDriver driver;
 	private static final Logger logger = LogManager.getLogger(WebDriverFactory.class);
+	private static final int IMPLICIT_TIMEOUT_TIME = 5;
 
 	/**
 	 * инициализация драйвера без опций
+	 *
 	 * @param webDriverName - имя драйвер
 	 * @return - драйвер
 	 */
 	public static WebDriver create(String webDriverName) {
 		webDriverName = webDriverName.toUpperCase().replaceAll("'", "");
-		logger.info(String.format("BROWSER IS %s",webDriverName));
+		logger.info(String.format("BROWSER IS %s", webDriverName));
 
 		Browsers browser = Browsers.valueOf(webDriverName);
+
+		WebDriver driver;
 
 		switch (browser) {
 			case CHROME:
@@ -39,36 +42,40 @@ public class WebDriverFactory {
 				logger.info("Unknown browser name");
 				driver = null;
 		}
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT_TIME, TimeUnit.SECONDS);
 		logger.info(String.format("Return %s driver without options", webDriverName));
 		return driver;
 	}
 
 	/**
 	 * инициализация драйвера chrome с опциями
+	 *
 	 * @param webDriverName - имя драйвера
-	 * @param options - опции
+	 * @param options       - опции
 	 * @return - драйвер
 	 */
 	public static WebDriver create(String webDriverName, ChromeOptions options) {
 		logger.info(String.format("Return %s with options", webDriverName));
+		WebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT_TIME, TimeUnit.SECONDS);
 		return driver;
 	}
 
 	/**
 	 * инициализация драйвера firefox с опциями
+	 *
 	 * @param webDriverName - имя драйвера
-	 * @param options - опции
+	 * @param options       - опции
 	 * @return - драйвер
 	 */
 	public static WebDriver create(String webDriverName, FirefoxOptions options) {
 		logger.info(String.format("Return %s with options", webDriverName));
+		WebDriver driver;
 		WebDriverManager.firefoxdriver().setup();
 		driver = new FirefoxDriver(options);
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT_TIME, TimeUnit.SECONDS);
 		return driver;
 	}
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -15,14 +15,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 
-public class SampleTest {
-	private final Logger logger = LogManager.getLogger(SampleTest.class);
-	private final ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
-	private static WebDriver driver;
-	private Actions actions;
-	private WebDriverWait wait;
-	private JavascriptExecutor executor;
-	//локаторы
+public class SampleTest extends BaseTest{
+
+	/**
+	 * Локаторы
+	 */
 	private final By electronics = By.xpath("//span[contains(text(),\"Электроника\")]");
 	private final By smartphones = By.xpath("//a[contains(text(),\"Смартфоны\")]");
 	private final By samsung = By.xpath("//span[text() = \"Samsung\"]");
@@ -30,62 +27,34 @@ public class SampleTest {
 	private final By sortByPrice = By.cssSelector("[data-autotest-id=\"dprice\"]");
 	private final By firstSamsung = By.xpath("//span[contains(text(),\"Samsung\")]/../../../../.." +
 			"//div[contains(@aria-label,\"сравнению\")]");
-	private final By accept = By.xpath("//*[text()=\"Понятно\"]/..");
 	private final By added = By.xpath("//*[contains(text(),\"добавлен к сравнению\")]");
 	private final By firstXiaomi = By.xpath("//span[contains(text(),\"Xiaomi\")]/../../../../.." +
 			"//div[contains(@aria-label,\"сравнению\")]");
 	private final By compare = By.xpath("//span[text() = \"Сравнить\"]/..");
 	private final By elementInList = By.xpath("//*[@class = \"LwwocgVx0q _2VGDFjE-Ev\"]");
 
-
-	@Before
-	public void setUp() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("start-maximized");
-		driver = WebDriverFactory.create(CHROME, options);
-		actions = new Actions(driver);
-		wait = new WebDriverWait(driver, 5);
-		executor = (JavascriptExecutor)driver;
-	}
-
 	@Test
 	public void yandexMarketTest() {
+		logger.info("Start test");
 		driver.get(cfg.urlYandex());
-		driver.findElement(electronics).click();
-		driver.findElement(smartphones).click();
-		driver.findElement(smartphones).click();
-		driver.findElement(samsung).click();
-		driver.findElement(xiaomi).click();
-		driver.findElement(sortByPrice).click();
-		driver.findElement(accept).click();
+		clickToElement(electronics);
+		clickToElement(smartphones);
+		clickToElement(smartphones);
+		clickToElement(samsung);
+		clickToElement(xiaomi);
+		clickToElement(sortByPrice);
 //		добавляем samsung в сравнение
 		moveToElement(firstSamsung);
 		wait.until(ExpectedConditions.stalenessOf(driver.findElement(firstSamsung)));
-		driver.findElement(firstSamsung).click();
+		clickToElement(firstSamsung);
 		Assert.assertTrue(driver.findElement(added).getText().contains("Samsung"));
 //		добавляем xiaomi в сравнение
 		moveToElement(firstXiaomi);
-		driver.findElement(firstXiaomi).click();
+		clickToElement(firstXiaomi);
 		Assert.assertTrue(driver.findElement(added).getText().contains("Xiaomi"));
 //		переход в сравнение
 		executor.executeScript("arguments[0].click();", driver.findElement(compare));//обычный клик работает с ошибкой
-		//element click intercepted, нужно подобрать ожидание
+//		element click intercepted, нужно подобрать ожидание
 		Assert.assertEquals(2, driver.findElements(elementInList).size());
-	}
-
-	@After
-	public void setDown() {
-		if (driver != null) {
-			driver.quit();
-			logger.info("Драйвер успешно закрыт");
-		}
-	}
-
-	/**
-	 * метод позволяет навести курсор на элемент
-	 * @param by - локатор элемента
-	 */
-	private void moveToElement(By by) {
-		actions.moveToElement(driver.findElement(by)).build().perform();
 	}
 }
